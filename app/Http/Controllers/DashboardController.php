ed9691136bc5953690f0400bf428e03fda335c0f
lanang_s <lanang_s@apps.ipb.ac.id>
2020-05-15 23:20:48 +0700

    fungsi tambah matpel buat siswa

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,8 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\MataPelajaran;
+use App\MatpelSiswa;
+use App\MatpelGuru;
+use App\Programs;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class DashboardController extends Controller
 {
@@ -14,5 +19,33 @@ class DashboardController extends Controller
     public function index()
     {
         return view('/home');
+    }
+
+    public function getMatpelList()
+    {
+        //get semua matpel di tabel mata pelajaran
+        $mata_pelajaran = MataPelajaran::all();
+        
+        return view('/layouts/dashboard/tambahMatpel',compact('mata_pelajaran'));
+    }
+
+    public function tambahMatpel(Request $request)
+    {
+        //tambah matpel ke tabel mata pelajaran siswa
+        $tambahMatpel = MatpelSiswa::firstOrCreate([        
+            'mata_pelajaran_id' => $request->input('tambahMatpel'),
+            'siswa_id' => Auth::user()->id,
+
+        ]);
+        
+        
+        // $tambahProgram = Programs::firstOrCreate([
+        //     'id_guru' => MatpelGuru::where('id_matpel', $request->input('tambahMatpel'))->get(),
+        //     'id_matpel' => $request->input('tambahMatpel'),
+        //     'id_siswa' => Auth::user()->id,
+
+        // ]);
+        
+        return back();
     }
 }
