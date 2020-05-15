0e1cca77adb63cd44c3329f1e61c75c805c6d8d5
juliuschris <julius_chris@apps.ipb.ac.id>
2020-05-15 16:23:21 +0700

    get data pertanyaan ke view epbm

--- database/seeds/PertanyaanSeeder.php
+++ database/seeds/PertanyaanSeeder.php
@@ -13,31 +13,24 @@ class PertanyaanSeeder extends Seeder
     {
          //insert data
          DB::table('pertanyaan')->insert([
-        	'id' => '1',
             'list_pertanyaan' => 'Mengacu pada KD yang ingin dicapai'
         ]);
         DB::table('pertanyaan')->insert([
-        	'id' => '2',
             'list_pertanyaan' => 'Memuat proses dan gambaran hasil belajar yang ingin dicapai'
         ]);
         DB::table('pertanyaan')->insert([
-        	'id' => '3',
             'list_pertanyaan' => 'Sesuai dengan kebutuhan belajar siswa'
         ]);
         DB::table('pertanyaan')->insert([
-        	'id' => '4',
             'list_pertanyaan' => 'Relevan untuk mencapai tujuan pembelajaran'
         ]);
         DB::table('pertanyaan')->insert([
-        	'id' => '5',
             'list_pertanyaan' => 'Memudahkan pemahaman siswa'
         ]);
         DB::table('pertanyaan')->insert([
-        	'id' => '6',
             'list_pertanyaan' => 'Variatif'
         ]);
         DB::table('pertanyaan')->insert([
-        	'id' => '7',
             'list_pertanyaan' => 'Melibatkan siswa'
         ]);
     }
