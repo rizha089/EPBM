2163b083b5b5bf4198545c469cc83c3d03d6c760
lanang_s <lanang_s@apps.ipb.ac.id>
2020-05-18 21:15:16 +0700

    admin regis login listSiswa listGuru listMatpel

---
+++ app/Http/Controllers/Auth/AdminController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Admin;
+use App\Siswa;
+use App\Guru;
+use App\MataPelajaran;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+
+class AdminController extends Controller
+{
+    use AuthenticatesUsers;
+
+    protected $guard = 'admin';
+
+    //protected $redirectTo = '/home';
+
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.adminLogin');
+    }
+
+    public function guard()
+    {
+        return auth()->guard('admin');
+    }
+
+    public function showRegisterPage()
+    {
+        return view('auth.adminRegister');
+    }
+
+    public function register(Request $request)
+    {
+        $this->validate($request,[
+            'username' => 'required|string|max:199',
+            'password' => 'required|string|confirmed'
+        ]);
+        $admin=Admin::create([
+            'username' => $request->username,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return redirect()->route('admin-login')->with('success','Registration Success');
+    }
+
+    public function login(Request $request)
+    {
+        if (auth()->guard('admin')->attempt(['username' => $request->username, 'password' => $request->password ])) {
+            return redirect()->route('admin');
+        }
+
+
+        return back()->withErrors(['username' => 'username or password are wrong.']);
+    }
+
+    public function daftarsiswa(){
+
+        $daftarSiswa = Siswa::all();
+
+        return view('layouts/dashboard/daftarsiswa', compact('daftarSiswa'));
+    }
+
+    public function siswaDelete($id){
+        $siswa = Siswa::find($id);
+        $siswa -> delete();
+        
+        return back();
+    }
+
+    public function daftarguru(){
+
+        $daftarGuru = Guru::all();
+
+        return view('layouts/dashboard/daftarguru', compact('daftarGuru'));
+    }
+
+    public function guruDelete($id){
+        $guru = Guru::find($id);
+        $guru -> delete();
+        
+        return back();
+    }
+
+    public function daftarMatpel(){
+
+        $daftarMatpel = MataPelajaran::all();
+
+        return view('layouts/dashboard/daftarmatpel', compact('daftarMatpel'));
+    }
+
+    public function matpelDelete($id){
+        $matpel = MataPelajaran::find($id);
+        $matpel -> delete();
+        
+        return back();
+    }
+
+}
---
+++ resources/views/Admin.blade.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Dashboard | SCB - @yield('title')</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <link rel="apple-touch-icon" sizes="76x76" href="klorofil/img/logoDoang.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="klorofil/img/logoDoang.png">
+
+    @section('css')
+        <!-- CSS -->
+        {{ Html::style('klorofil/css/bootstrap.min.css')}}
+        {{ Html::style('klorofil/css/vendor/icon-sets.css')}}
+        {{ Html::style('klorofil/css/main.min.css')}}
+        {{ Html::style('klorofil/css/demo.css')}}
+
+        @yield('extra-style')
+
+        <!-- GOOGLE FONTS -->
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+    @show
+</head>
+
+<body>
+    <!-- WRAPPER -->
+    <div id="wrapper">
+        <!-- SIDEBAR -->
+        <div class="sidebar">
+            <div class="brand">
+                <a href="/admin"><img src="/klorofil/img/logoSCB.png" alt="Logo" class="img-responsive logo"></a>
+            </div>
+            <div class="sidebar-scroll">
+                <nav>
+                    <ul class="nav">
+
+                        <li><a href="{{route('admin')}}" {{Route::is('admin')?'class=active':''}}><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
+                        <li><a href="{{route('daftarsiswa')}}" {{Route::is('daftarsiswa')?'class=active':''}}><i class="lnr lnr-code"></i> <span>Daftar Siswa</span></a></li>
+                        <li><a href="{{route('daftarguru')}}" {{Route::is('daftarguru')?'class=active':''}}><i class="lnr lnr-user"></i> <span>Daftar Guru</span></a></li>
+                        <li><a href="{{route('daftarmatpel')}}" {{Route::is('daftarmatpel')?'class=active':''}}><i class="lnr lnr-user"></i> <span>Daftar Mata Pelajaran</span></a></li>
+                        
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+        <!-- END SIDEBAR -->
+        <!-- MAIN -->
+        <div class="main">
+            <!-- NAVBAR -->
+            @include('layouts/partials/header')
+            <!-- END NAVBAR -->
+            <!-- MAIN CONTENT -->
+            <div class="main-content">
+                @yield('content')
+
+            <div class="container-fluid">
+                <!-- OVERVIEW -->
+                <div class="panel panel-headline">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Selamat Datang,</h3>
+                    <h3 class="panel-title"><b> Admin </b></h3> 
+                    </div>
+                    
+            </div>
+
+                   
+            </div>
+            <!-- END MAIN CONTENT -->
+            <footer>
+                @include('layouts/partials/footer')
+            </footer>
+        </div>
+        <!-- END MAIN -->
+    </div>
+    <!-- END WRAPPER -->
+
+    @section('script')
+    <!-- Javascript -->
+        {{ Html::script('klorofil/js/jquery/jquery-2.1.0.min.js') }}
+        {{ Html::script('klorofil/js/bootstrap/bootstrap.min.js') }}
+        {{ Html::script('klorofil/js/plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}
+        {{ Html::script('klorofil/js/plugins/jquery-easypiechart/jquery.easypiechart.min.js') }}
+        {{ Html::script('klorofil/js/plugins/chartist/chartist.min.js') }}
+        {{ Html::script('klorofil/js/klorofil.min.js') }}
+
+        @yield('extra-script')
+    @show
+
+
+</body>
+
+</html>
