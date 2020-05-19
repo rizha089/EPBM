e673008f471774c3e54c970d15065b264d0f1d2f
khalidmeister <khaliddotdev@gmail.com>
2020-05-19 21:24:57 +0700

    Update GuruPage

--- app/Http/Controllers/GuruController.php
+++ app/Http/Controllers/GuruController.php
@@ -6,22 +6,55 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\RatePertanyaan;
+use App\Http\Controllers\Auth;
+use App\Rekap;
+use App\RekapDatabase;
 
 class GuruController extends Controller
 {
     //
     public function index() {
-        $rekap = DB::table('rekap')
-                    ->join('guru', 'rekap.id_guru', '=', 'guru.id')
-                    ->join('mata_pelajaran', 'rekap.id_matpel', '=', 'mata_pelajaran.id')
-                    ->join('pertanyaan', 'rekap.id_pertanyaan', '=', 'pertanyaan.id')
-                    ->where('rekap.id_guru', '1')
-                    ->select('guru.nama as nama_guru', 'mata_pelajaran.nama_matpel as nama_matpel', 'pertanyaan.list_pertanyaan as pertanyaan', 'nilai', DB::raw('count(nilai) as count_nilai'))
-                    ->groupBy('nama_guru', 'nama_matpel', 'pertanyaan', 'nilai')
-                    ->get();
-        // $pertanyaan = DB::table('pertanyaan')->get();
-        // return $rekap->toJson(JSON_PRETTY_PRINT);
-        // echo ;
-        return view('guruPage', ['rekap' => $rekap]);
+        // Buat Dapetin Pertanyaan
+        $pertanyaans_db = DB::table('pertanyaan')->select('list_pertanyaan')->get();
+        $pertanyaan = [];
+        foreach($pertanyaans_db as $p) {
+            array_push($pertanyaan, $p->list_pertanyaan);
+        }
+
+        $matpel_db = DB::table('matpel_guru')
+                        -> join('mata_pelajaran', 'matpel_guru.id_matpel', '=', 'mata_pelajaran.id')
+                        -> select('mata_pelajaran.nama_matpel')
+                        -> where('matpel_guru.id_guru', auth()->user()->id)
+                        -> get();
+        $matpel = [];
+        foreach($matpel_db as $m) {
+            array_push($matpel, $m->nama_matpel);
+        }
+
+        $nilai= array(array(array()));
+        // Per Matpel
+        for($i = 0; $i < 1; $i++) {
+            // Per Pertanyaan
+            for($j = 0; $j < 7; $j++){
+                $temp = DB::table('rekap')
+                            -> join('guru', 'rekap.id_guru', '=', 'guru.id')
+                            -> join('mata_pelajaran', 'rekap.id_matpel', '=', 'mata_pelajaran.id')
+                            -> join('pertanyaan', 'rekap.id_pertanyaan', '=', 'pertanyaan.id')
+                            -> where('rekap.id_guru', auth()->user()->id)
+                            -> groupBy('guru.nama', 'mata_pelajaran.nama_matpel', 'pertanyaan.list_pertanyaan', 'nilai')
+                            -> select(DB::raw('count(nilai) as count_nilai'))
+                            -> where('rekap.id_pertanyaan', $j+1)
+                            -> get();
+
+                $temp_nilai = [];
+                foreach($temp as $t) {
+                    array_push($temp_nilai, $t->count_nilai);
+                }
+                $nilai[$i][$j][] = $temp_nilai;
+            }
+        }
+
+        // return json_encode($nilai[0][0]);
+        return view('guruPage2', ['pertanyaan' => $pertanyaan, 'matpel' => $matpel, 'nilai' => $nilai]);
     }
 }
--- resources/views/guruPage2.blade.php
+++ resources/views/guruPage2.blade.php
@@ -86,72 +86,8 @@
                                     <div class="col-md-3">
                                         <div class="metric">
                                             <p>Pertanyaan: {{ $pertanyaan[$j] }}</p>
-                                            <svg></svg>
-                                            <script src="https://d3js.org/d3.v4.min.js"></script>
-                                            <script>
-                                                const margin = 60;
-                                                const width = 300 - 2 * margin;
-                                                const height = 200 - 2 * margin;
-                                                // var svgWidth = 500;
-                                                // var svgHeight = 300;
 
-                                                var svg = d3.select('svg')
-                                                    .attr("width", 300)
-                                                    .attr("height", 200)
-                                                    // .attr("class", "bar-chart");
 
-                                                const chart = svg.append('g')
-                                                    .attr('transform', `translate(${margin}, ${margin})`);
-
-                                                const yScale = d3.scaleLinear()
-                                                    .range([height, 0])
-                                                    .domain([0, 100])
-
-                                                const xScale = d3.scaleBand()
-                                                    .range([0, width])
-                                                    .domain(['N1', 'N2', 'N3', 'N4', 'N5'])
-                                                    .padding(0.2)
-
-                                                var list = <?php echo json_encode($nilai[$i][$j][0]) ?>;
-
-                                                var pertanyaans = [
-                                                    { nilai: "N1", jumlah: list[0]},
-                                                    { nilai: "N2", jumlah: list[1]},
-                                                    { nilai: "N3", jumlah: list[2]},
-                                                    { nilai: "N4", jumlah: list[3]},
-                                                    { nilai: "N5", jumlah: list[4]},
-                                                ];
-
-                                                chart.append('g')
-                                                    .call(d3.axisLeft(yScale));
-
-                                                chart.append('g')
-                                                    .attr('transform', `translate(0, ${height})`)
-                                                    .call(d3.axisBottom(xScale));
-
-                                                chart.selectAll()
-                                                    .data(pertanyaans)
-                                                    .enter()
-                                                    .append("rect")
-                                                    .attr("x", (s) => xScale(s.nilai))
-                                                    .attr("y", (s) => yScale(s.jumlah))
-                                                    .attr('height', (s) => height - yScale(s.jumlah))
-                                                    .attr('width', xScale.bandwidth())
-                                                    .style('fill', '#045c3c')
-
-                                                svg.append('text')
-                                                    .attr('x', -(height / 2) - margin)
-                                                    .attr('y', margin / 2.4)
-                                                    .attr('transform', 'rotate(-90)')
-                                                    .attr('text-anchor', 'middle')
-                                                    .text('Jumlah')
-
-                                                svg.append('text')
-                                                    .attr('x', width / 2 + margin)
-                                                    .attr('y', 40)
-                                                    .attr('text-anchor', 'middle')
-                                                    .text('Nilai yang didapatkan')
-                                            </script>
                                         </div>
                                     </div>
                                 </div>
